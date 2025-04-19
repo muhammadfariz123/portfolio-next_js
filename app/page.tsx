8fe96dc8d1a27d41a786ee05cb439b9f821a6884
muhammadfariz123 <warungkombas@gmail.com>
2025-04-19 17:42:08 +0700

    set up

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Lanyard from "./components/Lanyard/Lanyard";
+export default function Home() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <div className="container border mx-auto h-screen">
+        <div className="grid grid-cols-12">
+          <div className="col-span-6">A</div>
+          <div className="col-span-6">B</div>
+        </div>
+      </div>
+    </div>
+  );
+}
